d7d5731a5ac3357b5a99d037bcc802db13467005
brwillian <wiliam-m-@hotmail.com>
2022-11-19 01:51:52 -0300

    :alembic: improved new tests

--- main.py
+++ main.py
@@ -2,10 +2,15 @@ import cv2
 import os
 import numpy as np
 from dataset import DatasetLoader
+from utils import Utils
 
 
 def main():
-    loader = DatasetLoader('')
+    images = Utils.get_imgs_from_directory("./dataset/CRACK500/traindata/")
+    print(images)
+    loader = DatasetLoader(
+
+    )
     for root, path, files in os.walk("./dataset/CRACK500/traindata/"):
         for file in files:
             if file.endswith(".jpg"):
@@ -14,15 +19,15 @@ def main():
                 img = cv2.resize(img, (500, 500))
                 cv2.imshow("img", img)
                 cv2.waitKey(0)
-                # img = loader.randomHueSaturationValue(img,
-                #                                       hue_shift_limit=(
-                #                                       np.random.uniform(-25, -100), np.random.uniform(25, 100)),
-                #                                       sat_shift_limit=(
-                #                                       np.random.uniform(-45, 0), np.random.uniform(0, 45)),
-                #                                       val_shift_limit=(
-                #                                       np.random.uniform(-50, 0), np.random.uniform(0, 50))
-                #                                       )
-                img = loader.brightness_range(img)
+                img = loader.randomHueSaturationValue(img,
+                                                      hue_shift_limit=(
+                                                      np.random.uniform(-25, -100), np.random.uniform(25, 100)),
+                                                      sat_shift_limit=(
+                                                      np.random.uniform(-45, 0), np.random.uniform(0, 45)),
+                                                      val_shift_limit=(
+                                                      np.random.uniform(-50, 0), np.random.uniform(0, 50))
+                                                      )
+                img = loader.adjust_gamma(img)
                 cv2.imshow("img", img)
                 cv2.waitKey(0)
 
